2a2b4f32ac20063666c0f7f67178388853148358
Chloe <chloesman1999@gmail.com>
2023-03-05 01:22:24 +0000

    add

--- Pokemon-Pack-Opening/src/App.jsx
+++ Pokemon-Pack-Opening/src/App.jsx
@@ -10,17 +10,22 @@ import Pokemoncard from './Components/Pokemoncard'
 function App() {
 
 
-const [pokemonCard, setPokemonCard] = useState(false) 
+const [pokemonCards, setPokemonCards] = useState([]) 
 const URL = "https://api.pokemontcg.io/v2/cards/base1-"
 useEffect(() => {
   function getPokemon(){ 
+    const pokemon =[] 
+  for (let i = 0; i < 10; i++){
     fetch(`${URL}${Math.floor(Math.random() * 102 +1)}`).then(res => res.json()).then(data => {
      console.log(data.data) 
-      setPokemonCard(data.data) 
+     pokemon.push(data.data)
+     if(pokemon.length ===10){setPokemonCards(pokemon)}
     })
   }
-     getPokemon()
+}
+  getPokemon()
 },[])
+
 
 const getCardPrice = (card) => {
   const cardPrice = card.tcgplayer.prices.normal !== undefined ? 
@@ -29,9 +34,13 @@ const getCardPrice = (card) => {
   return cardPrice;
 }
 
+if(pokemonCards.length === 0)
+{return (<div> Loading </div>)}
+
   return (
     <div className="App">
-      {pokemonCard && <Pokemoncard image={pokemonCard.images.small} name={pokemonCard.name} rarity={pokemonCard.rarity} price={getCardPrice(pokemonCard)}/>}
+     { /* {pokemonCard && <Pokemoncard image={pokemonCard.images.small} name={pokemonCard.name} rarity={pokemonCard.rarity} price={getCardPrice(pokemonCard)}/>} */ }
+    {pokemonCards.map((card, i)=> <Pokemoncard image={card.images.small} name={card.name} rarity={card.rarity} price={getCardPrice(card)} key={i} />)}
     </div>
   )
     
