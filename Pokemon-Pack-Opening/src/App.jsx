85e8dfd614126a8ab81c7d1861b071278c129c33
Chloe <chloesman1999@gmail.com>
2023-03-10 15:25:42 +0000

    2nd pack

--- Pokemon-Pack-Opening/src/App.jsx
+++ Pokemon-Pack-Opening/src/App.jsx
@@ -3,65 +3,91 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import Pokemoncard from './Components/Pokemoncard';
 import BaseSet from './assets/base_set.jpg';
-
+import SilverTempest from './assets/SilverTempest.jpg';
 
 function App() {
   const [pokemonCards, setPokemonCards] = useState([]);
-  const [showCards, setShowCards] = useState(false);
   const [clickedCard, setClickedCard] = useState(null);
-  const [showImage, setShowImage] = useState(false);
-  const URL = "https://api.pokemontcg.io/v2/cards/base1-";
-
-  useEffect(() => {
-    fetchPokemon();
-  }, []);
+  const [showImage, setShowImage] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+  const [points, setPoints] = useState(50); // Initialize points to 50
+  const URL_BASE_SET = "https://api.pokemontcg.io/v2/cards/base1-";
+  const URL_SILVER_TEMPEST = "https://api.pokemontcg.io/v2/cards/swsh12-";
 
   const getCardPrice = (card) => {
-    const cardPrice =
-      card.tcgplayer.prices.normal !== undefined
-        ? card.tcgplayer.prices.normal.market
-        : card.tcgplayer.prices['1stEditionHolofoil'] !== undefined
-        ? card.tcgplayer.prices['1stEditionHolofoil'].market
-        : card.tcgplayer.prices.holofoil.market;
+    if (!card) {
+      return 0; // return 0 if card is undefined
+    }
+
+    let cardPrice;
+    if (card.tcgplayer && card.tcgplayer.prices) {
+      cardPrice =
+        card.tcgplayer.prices.normal !== undefined
+          ? card.tcgplayer.prices.normal.market
+          : card.tcgplayer.prices['1stEditionHolofoil'] !== undefined
+          ? card.tcgplayer.prices['1stEditionHolofoil'].market
+          : card.tcgplayer.prices.holofoil.market;
+    } else {
+      cardPrice = 0; // set card price to 0 if prices are not available
+    }
     return cardPrice;
   };
 
-  const fetchPokemon = () => {
+  const fetchPokemon = (url) => {
+    const availableCards = Array.from(Array(102).keys());
     const pokemon = [];
     for (let i = 0; i < 10; i++) {
-      fetch(`${URL}${Math.floor(Math.random() * 102 + 1)}`)
+      const randomIndex = Math.floor(Math.random() * availableCards.length); // select a random index from the availableCards array
+      const cardNumber = availableCards[randomIndex]; // get the card number at the selected index
+      availableCards.splice(randomIndex, 1); // remove the card number from the availableCards array
+
+      fetch(`${url}${cardNumber}`)
         .then((res) => res.json())
         .then((data) => {
           console.log(data.data);
           pokemon.push(data.data);
           if (pokemon.length === 10) {
             setPokemonCards(pokemon);
+            setShowButton(true);
+            const totalCardPrices = pokemon.reduce((sum, card) => sum + getCardPrice(card), 0);
+            setPoints(points + totalCardPrices - 50);
           }
         });
     }
   };
 
-  const handleCardClick = (index) => {
-    setClickedCard(index);
-  }
+  const handleBaseSetClick = () => {
+    setShowImage(false);
+    setClickedCard(null); // Reset clickedCard before fetching new cards
+    fetchPokemon(URL_BASE_SET);
+  };
+
+  const handleSilverTempestClick = () => {
+    setShowImage(false);
+    setClickedCard(null); // Reset clickedCard before fetching new cards
+    fetchPokemon(URL_SILVER_TEMPEST);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPoints(points => points + 1);
+    }, 1000); // Update points every second
+    return () => clearInterval(intervalId); // Cleanup function to clear interval
+  }, []);
 
   return (
     <div>
-      {showImage ? (
-        <div
-          className="image-container"
-          onClick={() => setShowImage(false)}
-        >
-          <img src={BaseSet} alt="pack" />
-        </div>
-      ) : (
-        <div className="pack-container" onClick={() => setShowCards(true)}>
-          <img src={BaseSet} alt="pack" className="pack-image" />
-          <div className="pack-text">Click to open</div>
-        </div>
-      )}
+      <p>Points: {points}</p>
+      <div className="pack-container" onClick={() => handleBaseSetClick()}>
+  <img src={BaseSet} alt="base set pack" className="pack-image" />
+</div>
+<div className="pack-container" onClick={() => handleSilverTempestClick()}>
+  <img src={SilverTempest} alt="silver tempest pack" className="pack-image" />
+</div>
+      
 
-      {showCards && (
+
+      {!showImage && (
         <div className="card-container">
           {pokemonCards.map((card, i) => (
             <Pokemoncard
@@ -71,16 +97,22 @@ function App() {
               price={getCardPrice(card)}
               key={i}
               style={{
-                zIndex: clickedCard === i ? 1 : (i * -1) + 1,
+                zIndex: pokemonCards.length - i + 10,
                 transform: clickedCard === i ? 'scale(1.1)' : 'none',
                 transition: 'transform 0.3s ease-in-out'
               }}
+
               onClick={() => handleCardClick(i)}
             />
           ))}
+        
+          {showButton && ( // Render button only if showButton is true
+            <button onClick={() => setShowImage(true)}>Go Back Packs</button>
+          )}
         </div>
       )}
     </div>
   );
 }
+
 export default App;
--- Pokemon-Pack-Opening/src/Components/Pokemoncard.jsx
+++ Pokemon-Pack-Opening/src/Components/Pokemoncard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const Pokemoncard = ({ image, name, rarity, price, style }) => {
   return (
-    <div className="card" style={{ ...style, position: 'absolute', center: `${style.zIndex * 20}px` }}>
+    <div className="card" style={style}>
       <img src={image} alt={name} />
       <div className="card-info">
         <h3>{name}</h3>
         <p>Rarity: {rarity}</p>
-        <p>Price: ${price.toFixed(2)}</p>
+        <p>Points: {price.toFixed(2)}</p>
       </div>
     </div>
   );
 };
 
 export default Pokemoncard;
+
